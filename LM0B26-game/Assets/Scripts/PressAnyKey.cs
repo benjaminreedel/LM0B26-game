fd76bd4e20df27e27a7d7c819cabde4e440b361d
Benjamin Reedel <benjamin_reedel@sfu.ca>
2021-05-08 01:52:22 -0700

    Final Commit

--- LM0B26-game/Assets/Scripts/PressAnyKey.cs
+++ LM0B26-game/Assets/Scripts/PressAnyKey.cs
@@ -11,7 +11,7 @@ public class PressAnyKey : MonoBehaviour
     {
         if (Input.anyKeyDown)
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(1);
         }
         
     }
